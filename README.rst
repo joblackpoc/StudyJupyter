61f93410c12991c1a7218d867e234dffa4f07bcc
joblackpoc <duopoc@gmail.com>
2018-10-22 08:59:54 +0700

    add sport cmpo

---
+++ README.rst
@@ -0,0 +1,44 @@
+``pivottablejs``: the Python module
+===================================
+
+Drag’n’drop Pivot Tables and Charts for `Jupyter/IPython Notebook`_,
+care of `PivotTable.js`_
+
+Installation
+------------
+
+``pip install pivottablejs`` or ``conda install pivottablejs``
+
+**Note** in the past, the ``conda`` command above installed `pivottablejs-airgap`_ instead of this library; apologies for any confusion this change may cause.
+
+Usage
+-----
+
+.. code:: python
+
+    import pandas as pd
+    df = pd.read_csv("some_input.csv")
+
+    from pivottablejs import pivot_ui
+
+    pivot_ui(df)
+
+Advanced Usage
+--------------
+
+Include any `JSON-serializable option to PivotTable.js's pivotUI() function`_ as a keyword argument.
+
+.. code:: python
+
+    pivot_ui(df, rows=['row_name'], cols=['col_name'])
+
+Independently control the output file path and the URL used to access it from Jupyter, in case the default relative-URL behaviour is incompatible with Jupyter's settings.
+
+.. code:: python
+
+    pivot_ui(df, outfile_path="/x/y.html", url="http://localhost/a/b/x.html")
+
+.. _Jupyter/IPython Notebook: http://jupyter.org/
+.. _PivotTable.js: https://github.com/nicolaskruchten/pivottable
+.. _pivottablejs-airgap: https://github.com/ContinuumIO/pivottablejs-airgap
+.. _JSON-serializable option to PivotTable.js's pivotUI() function: https://github.com/nicolaskruchten/pivottable/wiki/Parameters#options-object-for-pivotui
---
+++ pivottablejs.py
@@ -0,0 +1,81 @@
+# %install_ext http://nicolas.kruchten.com/pivottable/jupyter/pivottablejs.py
+# %load_ext pivottablejs
+# %pivottablejs data_frame
+
+__version__ = '2.21.0'
+
+
+template = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PivotTable.js</title>
+
+        <!-- external libs from cdnjs -->
+        <link rel="stylesheet" type="text/css" href="%(static)s/pivot-ajax/c3.min.css">
+        <script type="text/javascript" src="%(static)s/pivot-ajax/jquery.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivot-ajax/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivot-ajax/d3.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivot-ajax/jquery.csv-0.71.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivot-ajax/c3.min.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="%(static)s/pivottable/pivot.min.css">
+        <script type="text/javascript" src="%(static)s/pivottable/pivot.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivottable/d3_renderers.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivottable/c3_renderers.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivottable/export_renderers.min.js"></script>
+
+        <style>
+            body {font-family: Verdana;}
+            .node {
+              border: solid 1px white;
+              font: 10px sans-serif;
+              line-height: 12px;
+              overflow: hidden;
+              position: absolute;
+              text-indent: 2px;
+            }
+            .c3-line, .c3-focused {stroke-width: 3px !important;}
+            .c3-bar {stroke: white !important; stroke-width: 1;}
+            .c3 text { font-size: 12px; color: grey;}
+            .tick line {stroke: white;}
+            .c3-axis path {stroke: grey;}
+            .c3-circle { opacity: 1 !important; }
+        </style>
+    </head>
+    <body>
+        <script type="text/javascript">
+            $(function(){
+                if(window.location != window.parent.location)
+                    $("<a>", {target:"_blank", href:""})
+                        .text("[pop out]").prependTo($("body"));
+
+                $("#output").pivotUI(
+                    $.csv.toArrays($("#output").text()),
+                    {
+                        renderers: $.extend(
+                            $.pivotUtilities.renderers,
+                            $.pivotUtilities.c3_renderers,
+                            $.pivotUtilities.d3_renderers,
+                            $.pivotUtilities.export_renderers
+                            ),
+                        hiddenAttributes: [""]
+                    }
+                ).show();
+             });
+        </script>
+        <div id="output" style="display: none;">%(div)s</div>
+    </body>
+</html>
+"""
+
+from IPython.display import IFrame
+
+def pivot_ui(df, outfile_path = "pivottablejs.html", width="100%", height="500"):
+    # FIXME: we shouldn't hard-code the port here
+    port = 8888 # get_config().NotebookApp.port
+    static_path = 'http://localhost:%s' % port
+    with open(outfile_path, 'w') as outfile:
+        outfile.write(template % {'div': df.to_csv(),
+                                  'static': '%s/static' % static_path})
+    return IFrame(src=outfile_path, width=width, height=height)
